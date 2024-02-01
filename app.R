4ed7c2d1e188024d8198c2b263577a8f81a7437f
HollyGaudian <h.gaudian97@gmail.com>
2024-02-01 17:12:08 +0000

    updated, neater code, same issue

--- app.R
+++ app.R
@@ -23,12 +23,22 @@ library("shinyalert")
 library("DT")
 library("shinythemes")
 library("shinyWidgets")
+library("jsonlite")
 
-#source("https://github.com/dreamRs/shinyWidgets/blob/26838f9e9ccdc90a47178b45318d110f5812d6e1/R/setSliderColor.R")
+
+
+lenfreq <- read.csv(file = here("data/bansis_len_freq_data.csv")) |>
+  filter(sciname == "Harpadon nehereus") |>
+  mutate(len=floor(length_cm)) |>
+  group_by(len) |>
+  summarise(fq=sum(frequency)) |>
+  ungroup()
+
+data <- data.frame(len = lenfreq$len, fq = lenfreq$fq)
+print(data)
 
 initial_peak <- 20
 initial_slope <- 10
-
 
 
 ui <- fluidPage(
@@ -36,7 +46,6 @@ ui <- fluidPage(
   titlePanel("Plot Selectivity Curve"),
   sidebarLayout(
     sidebarPanel(
-      #setSliderColor("#474747;"),
       chooseSliderSkin(
         skin = "Flat",
         color = "#941414"),
@@ -71,20 +80,11 @@ ui <- fluidPage(
   )
 ))
 
+
 server <- function(input, output, session) {
 
   shinyjs::enable()
 
-  #read data file
-
-  lenfreq <- read.csv(file = here("data/bansis_len_freq_data.csv")) |>
-      filter(sciname == "Harpadon nehereus") |>
-      mutate(len=floor(length_cm)) |>
-      group_by(len) |>
-      summarise(fq=sum(frequency)) |>
-      ungroup()
-
-data <- data.frame(len = names(data), fq = data)
 
 #paraData <- read_xlsx(path =here("data/stock_parameters.xlsx"),
                           #sheet = "parms") |>
@@ -96,86 +96,117 @@ data <- data.frame(len = names(data), fq = data)
 
 #params <- reactiveValues(peak = initial_peak, slope = initial_slope)
 
-parameter_data <- reactiveVal(data.frame(peak = numeric(), slope = numeric()))
+  observeEvent(input$plot_btn, {
+    print("Rendering plot...")
+  })
 
-    ## selectivity selection
-add_curve <- function(selectivity, data) {
-  if (selectivity == "None") {
-    return(NULL)
-  } else if (selectivity == "Logistic") {
-    # Add logistic curve to plot
-    return(geom_smooth(aes(y = stat(count)), method = "glm", stat = "smooth", position = "identity",
-                       method.args = list(family = "binomial"), se = FALSE, color = "red"))
-  } else if (selectivity == "Double Normal") {
-    # Add double normal curve to plot (example parameters)
-    mu1 <- mean(data$len) - 5
-    mu2 <- mean(data$len) + 5
-    sigma <- sd(data$len)
-    return(
-      geom_function(fun = function(x) dnorm(x, mean = mu1, sd = sigma), aes(y = stat(count)), color = "red") +
-        geom_function(fun = function(x) dnorm(x, mean = mu2, sd = sigma), aes(y = stat(count)), color = "red")
-    )
-  }
-}
+  # Define the initial reactiveVal for data
+  dataplot <- reactiveVal(data)
 
-    ## 2. Create a plot ##
-  output$plot1 <- renderPlot({
-    mu <- input$peak_slider
-    sigma <- input$slope_slider
 
-    req(input$plot_btn)
-    #print("Rendering plot...")
-    #print(paste("peak:", params$peak, "Slope:", params$slope))
+  #selctivity function curve
+  add_curve <- function(selectivity, data) {
+    if (selectivity == "None") {
+      return(NULL)
+    } else if (selectivity == "Logistic") {
+      # Add logistic curve to plot
+      return(geom_smooth(aes(y = stat(count)), method = "glm", stat = "smooth", position = "identity",
+                         method.args = list(family = "binomial"), se = FALSE, color = "#941414"))
+    } else if (selectivity == "Double Normal") {
+      # Add double normal curve to plot (example parameters)
+      mu1 <- mean(data$len) - 5
+      mu2 <- mean(data$len) + 5
+      sigma <- sd(data$len)
 
-    ggplot(data, aes(x = len, y = fq)) +
-      geom_bar(stat = "identity", fill = "#0f2667", alpha = 0.7) +
-      add_curve(input$selectivity_functions, data) +
-      #geom_smooth(method = "glm", family = gaussian(link = "identity"), se = FALSE,
-                  #formula = y ~ dnorm(x, mean = mu, sd = sigma), color = "#941414", linetype = "solid", size = 1) +
-      labs(x = "Length", y = "Frequency") +
-      theme_minimal() +
-      theme(
-        #panel.background = element_rect(fill = "transparent", color = NA),
-        #plot.background = element_rect(fill = "transparent", color = NA),
-        panel.grid.major.y = element_blank(),
-        panel.grid.minor.y = element_blank(),
-        panel.grid.major.x = element_blank(),
-        panel.grid.minor.x = element_blank(),
-        axis.line = element_line(color = "black"),
-        axis.text = element_text(color = "black", size = 15),  # Adjust size as needed
-        axis.ticks = element_line(color = "black", size = 1),
-        axis.title = element_text(color = "black", size = 15)
+      curve_data <- data.frame(
+        x = seq(min(data$len), max(data$len), length.out = 100),
+        y = dnorm(seq(min(data$len), max(data$len), length.out = 100), mean = mu1, sd = sigma) +
+          dnorm(seq(min(data$len), max(data$len), length.out = 100), mean = mu2, sd = sigma)
       )
-  })
 
-
-  # Table to display click coordinates
-  output$parameter_table <- renderTable({
-    parameter_data()
-  })
-
-  # Button to save the latest parameters
-  observeEvent(input$save, {
-    new_row <- data.frame(
-      Peak_X = input$peak_slider,
-      Slope = input$slope_slider
-    )
-    parameter_data(rbind(parameter_data(), new_row))
-  })
-
-## remove row on actionButton click ##
-  observeEvent(input$remove_row, {
-    current_data <- parameter_data()
-    if (nrow(current_data) > 0) {
-      parameter_data(current_data[-nrow(current_data), ])
+      return(geom_line(data = curve_data, aes(x = x, y = y), color = "#941414"))
+    } else {
+      return(NULL)
     }
-  })
-  shinyjs::runjs(
-    '$("#peak_slider").css("background-color", "#941414"); $("#slope_slider").css("background-color", "#941414");'
-  )
+  }
+
+  # Define reactiveVal for parameter data
+  parameter_data <- reactiveVal(data.frame(peak = numeric(), slope = numeric()))
+
+
+## 2. Create a plot ##
+output$plot1 <- renderPlot({
+  mu <- input$peak_slider
+  sigma <- input$slope_slider
+
+
+  req(input$plot_btn)
+  print("Rendering plot...")
+
+  ggplot(dataplot(), aes(x = len, y = fq)) +
+    geom_bar(stat = "identity", fill = "#0f2667", alpha = 0.7) +
+    add_curve(input$selectivity_functions, dataplot()) +
+    #geom_smooth(method = "glm", family = gaussian(link = "identity"), se = FALSE,
+    #formula = y ~ dnorm(x, mean = mu, sd = sigma), color = "#941414", linetype = "solid", size = 1) +
+    labs(x = "Length", y = "Frequency") +
+    theme_minimal() +
+    theme(
+      #panel.background = element_rect(fill = "transparent", color = NA),
+      #plot.background = element_rect(fill = "transparent", color = NA),
+      panel.grid.major.y = element_blank(),
+      panel.grid.minor.y = element_blank(),
+      panel.grid.major.x = element_blank(),
+      panel.grid.minor.x = element_blank(),
+      axis.line = element_line(color = "black"),
+      axis.text = element_text(color = "black", size = 15),  # Adjust size as needed
+      axis.ticks = element_line(color = "black", size = 1),
+      axis.title = element_text(color = "black", size = 15)
+    )
+})
+
+    # observe save button
+    observeEvent(input$save, {
+      params <- isolate(c(peak = input$peak_slider, slope = input$slope_slider))
+      parameter_data(cbind(parameter_data(), params))
+      })
+
+
+    ## observe remove button ##
+    observeEvent(input$remove_row, {
+      current_data <- parameter_data()
+      if (nrow(current_data) > 0) {
+        parameter_data(current_data[-nrow(current_data), ])
+      }
+    })
+
+    # render parameter table
+    output$parameter_table <- renderTable({
+      parameter_data()
+    })
+
+    shinyjs::runjs(
+      '$("#peak_slider").css("background-color", "#941414"); $("#slope_slider").css("background-color", "#941414");'
+    )
 }
 
 shinyApp(ui = ui, server = server)
+
+
+  # Table to display click coordinates
+  #output$parameter_table <- renderTable({
+    #parameter_data()
+  #})
+
+  # Button to save the latest parameters
+  #observeEvent(input$save, {
+    #new_row <- data.frame(
+      #Peak_X = input$peak_slider,
+      #Slope = input$slope_slider
+    #)
+    #parameter_data(rbind(parameter_data(), new_row))
+  #})
+
+
 
     #each input is a factor so levels are consistent for plotting characteristics
   #click_x <- data.frame(x = input$plot_click$x,
@@ -240,13 +271,3 @@ shinyApp(ui = ui, server = server)
   #output$priors_table <- renderTable({
     #priors()
   #})
-
-
-
-
-
-
-
-
-
-

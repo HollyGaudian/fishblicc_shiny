7bd9130cbe063041585e3853fa6afd96e56bd519
Holly <143628779+HollyGaudian@users.noreply.github.com>
2024-05-14 17:11:40 +0100

    Delete www directory

--- www/custom_styles.js
+++
@@ -1,9 +0,0 @@
-// www/custom_styles.js
-
-$(document).ready(function() {
-  // Set the background color of the peak slider
-  $("#peak_slider").css("background-color", "#941414");
-
-  // Set the background color of the slope slider
-  $("#slope_slider").css("background-color", "#941414");
-});
